d5e9cd3f2920c6d8627ebb3dad3654f46217f606
adhrox97 <adcom3@hotmail.com>
2024-01-05 17:43:39 -0500

    palmistry

--- app/src/main/java/com/adhrox/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/adhrox/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -1,12 +1,17 @@
 package com.adhrox.horoscapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import com.adhrox.horoscapp.R
 import com.adhrox.horoscapp.databinding.FragmentPalmistryBinding
@@ -26,7 +31,7 @@ class PalmistryFragment : Fragment() {
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
-            //Start camera
+            startCamera()
         } else {
             Toast.makeText(
                 requireContext(),
@@ -40,10 +45,36 @@ class PalmistryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         if (checkCameraPermission()) {
             //Tiene permisos aceptados
-            //Start camera
+            startCamera()
         } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
+
+    }
+
+    private fun startCamera() {
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            }catch (e: Exception){
+                Log.e("adhrox", "Algo pasó ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
 
     }
 
